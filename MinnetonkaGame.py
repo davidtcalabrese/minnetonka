b58581e106dfd40342a0566cc9695d2fbb697eb7
EmbeddedMan <brian@schmalzhaus.com>
2021-04-13 20:10:37 -0500

    Added a new room, started updating text to be speciffic to SchmalzHaus.

--- MinnetonkaGame.py
+++ MinnetonkaGame.py
@@ -92,10 +92,34 @@ def bear_room():
     # else call game_over() function with the "reason" argument
     game_over("Don't you know how to type a number?")
 
+# outside room
+def outside_room():
+  # give some prompts
+  # '\n' is to print the line in a new line
+  print("\nYou are now outside of the house.")
+  print("You are facing the back yard.")
+  print("You can see some stairs leading to a deck above you and a hot tub in front of you.")
+  print("What would you like do? (1 or 2)")
+  print("1). Get in the hot tub.")
+  print("2). Climb up to the deck.")
+
+  # take input()
+  answer = input(">")
+  
+  if answer == "1":
+    # the player is dead!
+    game_over("Ahh. That feels really realy nice.  You fall asleep in the hot tub.")
+  elif answer == "2":
+    # TODO: Go to the deck_room
+    print("\nYou are now on the deck. Enjoy the view.")
+  else:
+    # else call game_over() function with the "reason" argument
+    game_over("Don't you know how to type a number?")
+
 def start():
   # give some prompts.
-  print("\nYou are standing in a dark room.")
-  print("There is a door to your left and right, which one do you take? (l or r)")
+  print("\nYou are standing in Schmalz Haus. It is a room in Minnetonka.")
+  print("There is a closed door to your left, a sliding door stright ahead, and an open door to your right that looks like it leads to a hallway? (l, s or r)")
   
   # convert the player's input() to lower_case
   answer = input(">").lower()
@@ -103,6 +127,9 @@ def start():
   if "l" in answer:
     # if player typed "left" or "l" lead him to bear_room()
     bear_room()
+  elif "s" in answer:
+    # else if player typed "straight" or "s" lead him to outside_room()
+    outside_room()
   elif "r" in answer:
     # else if player typed "right" or "r" lead him to monster_room()
     monster_room()
